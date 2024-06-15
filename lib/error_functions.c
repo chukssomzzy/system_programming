56f2d72a5380e916d1e828d273b331df589c85c0
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-06-15 18:18:17 +0000

    Working on error functions

--- lib/error_functions.c
+++ lib/error_functions.c
@@ -1,7 +1,9 @@
 # include <stdarg.h>
+#include <stdio.h>
 # include "tlpi_hdr.h"
 # include "ename.c.inc"
 static void terminate(Boolean useExit3);
+static void outputError(Boolean, int, Boolean, const char *, va_list ap);
 
 # ifndef __GNUC__
 # define __attribute__ ((__noreturn__))
@@ -26,4 +28,72 @@ void terminate(Boolean useExit3)
 		_exit(EXIT_FAILURE);
 }
 
+/**
+ * outputError - format error message and print to standard error
+ * @useErr: boolean value to determine if the functions uses the
+ *	append the errorname and error message to the user string
+ * @err: integer that identifies the error
+ * @flushStdout: boolean that determines if to flush to stdout
+ * @format: format string for the user message
+ * @ap: user message list of arguments
+ */
+void outputError(Boolean useErr, int err, Boolean flushStdout,
+		const char *format, va_list ap)
+{
+# define BUF_SIZE 500
+	char err_text[BUF_SIZE], user_msg[BUF_SIZE], buf[BUF_SIZE];
 
+	vsnprintf(user_msg, BUF_SIZE, format, ap);
+
+	if (useErr)
+		snprintf(err_text, BUF_SIZE, " [%s %s]",
+				((err > 0 && err < MAX_ENAME) ? ename[err] : "?UNKNOWN?"),
+				strerror(err));
+	else
+		snprintf(err_text, BUF_SIZE, ":");
+
+	snprintf_nowarn(buf, BUF_SIZE, "ERROR%s %s\n", err_text, user_msg);
+
+	if (flushStdout)
+		fflush(stdout);
+	fputs(buf, stderr);
+	fflush(stderr);
+}
+
+/**
+ * errMsg - write formatted errormessage to stderr while flush both stdio and
+ *	stderr
+ * @format: string literal containing the format string
+ */
+void errMsg(const char *format, ...)
+{
+	va_list arg_list;
+	int saved_err;
+
+	saved_err = errno;
+
+	va_start(arg_list, format);
+	outputError(TRUE, errno, TRUE, format, arg_list);
+	va_end(arg_list);
+
+	errno = saved_err;
+}
+
+/**
+ * errExit - write formatted error message to stderr while flush both stdio and
+ *	stderr and exiting the process with exit(3)
+ * @format: literal format string
+ */
+void errExit(const char *format, ...)
+{
+	va_list arg_list;
+
+	va_start(arg_list, format);
+	outputError(TRUE, errno, TRUE, format, arg_list);
+	va_end(arg_list);
+
+	terminate(TRUE);
+}
+
+/**
+ * err_exit - write formatted error message to stderr and not flushing stdio
--- lib/tlpi_hdr.h
+++ lib/tlpi_hdr.h
@@ -13,7 +13,17 @@
 
 # include "error_functions.h"
 
-
+#ifndef __GNUC__
+# define snprintf_nowarn snprintf
+# else
+# define snprintf_nowarn(args...) __extension__({\
+		_Pragma("GCC diagnostic push"); \
+		_Pragma("GCC diagnostic ignored \"-Wformat-truncation\""); \
+		const int _snprintf_nowarn = snprintf(args); \
+		_Pragma("GCC diagnostic pop"); \
+		_snprintf_nowarn;\
+		})
+# endif
 typedef enum {
 	FALSE, TRUE
 } Boolean;
